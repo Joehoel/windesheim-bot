d826a872d833317c89b50c0346707166d6bac51a
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-06-14 16:03:45 +0200

    Updates

--- client/pages/api/auth/[...nextauth].ts
+++ client/pages/api/auth/[...nextauth].ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
-import { providers } from "next-auth/client";
 import Providers from "next-auth/providers";
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.Email({
       server: {

a07f9f2e5d972568963ece2105b1613be0122b92
Joehoel <31251240+Joehoel@users.noreply.github.com>
2021-06-14 12:14:58 +0200

    Things

--- client/pages/api/user/[id].ts
+++ client/pages/api/user/[id].ts
@@ -1,16 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import connect from "../../middleware/mongo";
-import User from "../../models/User";
+import connect from "../../../middleware/mongo";
+import User from "../../../models/User";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const id = req.params.id as string;
+  const id = req.query.id as string;
 
-  const { nickname } = await User.findOne({
+  const user = await User.findOne({
     id,
   });
 
-  if (nickname) {
-    return res.status(200).json({ nickname });
+  if (user) {
+    return res.status(200).json(user);
   }
 
   return res.status(500).json({ message: `No user found with the id: '${id}'` });
